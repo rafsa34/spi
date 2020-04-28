246da117dc72145d4b455981f33fa79dde22a2ec
Rafsa271 <blografsa@gmail.com>
2020-04-28 07:57:12 +0700

    perubahan di DashboardController.php dan dashboard/index.blade.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,32 +6,173 @@ use Illuminate\Http\Request;
 use \App\Lhp;
 use \App\Bagian;
 use \App\Temuan;
+use \App\Rekomendasi;
+use DB;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-    	$lhprutin = Lhp::where('jenis_monitor','LIKE','rutin')->count();
-    	$lhpkhusus = Lhp::where('jenis_monitor','LIKE','khusus')->count();
-    	$lhpspi = Lhp::where('jenis_monitor','LIKE','spi')->count();
-    	$lhpbpkp = Lhp::where('jenis_monitor','LIKE','bpkp')->count();
-    	$lhpbpk = Lhp::where('jenis_monitor','LIKE','bpk')->count();
-    	$lhpkap = Lhp::where('jenis_monitor','LIKE','kap')->count();
+
+        // $rekomendasiBelumSelesai = Rekomendasi::where('status','LIKE','Belum Selesai')->count();
+        // $rekomendasiSelesai = Rekomendasi::where('status','LIKE','Selesai')->count();
+        // $rekomendasiBelumDitindaklanjuti = Rekomendasi::where('status','LIKE','Belum Ditindaklanjuti')->count();
+        // $rekomendasiTidakDapatDitindaklanjuti = Rekomendasi::where('status','LIKE','Tidak Dapat Ditindaklanjuti')->count();
+
+        $query = "SELECT count(*) as total FROM `bagian` join lhp on lhp.bagian_id = bagian.id join rekomendasi on rekomendasi.lhp_id = lhp.id where nama_bagian = ? and rekomendasi.status = ?";
+        
+        //Sekretaris Perusahaan
+        //Belum Selesai
+        $SPBelumSelesai = DB::select($query, ['Sekretaris Perusahaan', 'Belum Selesai']);
+        $SPBelumSelesai = $SPBelumSelesai[0]->total;
+        //Selesai
+        $SPSelesai = DB::select($query, ['Sekretaris Perusahaan', 'Selesai']);
+        $SPSelesai = $SPSelesai[0]->total;
+        //Belum Ditindaklanjuti
+        $SPBelumDitindaklanjuti = DB::select($query, ['Sekretaris Perusahaan', 'Belum Ditindaklanjuti']);
+        $SPBelumDitindaklanjuti = $SPBelumDitindaklanjuti[0]->total;
+        //Tidak Dapat Ditindaklanjuti
+        $SPTidakDapatDitindaklanjuti = DB::select($query, ['Sekretaris Perusahaan', 'Tidak Dapat Ditindaklanjuti']);
+        $SPTidakDapatDitindaklanjuti = $SPTidakDapatDitindaklanjuti[0]->total;
+
+        //Satuan Pengawaasan Internal
+        //Belum Selesai
+        $SPIBelumSelesai = DB::select($query, ['Satuan Pengawasan Internal', 'Belum Selesai']);
+        $SPIBelumSelesai = $SPIBelumSelesai[0]->total;
+        //Selesai
+        $SPISelesai = DB::select($query, ['Satuan Pengawasan Internal', 'Selesai']);
+        $SPISelesai = $SPISelesai[0]->total;
+        //Belum Ditindaklanjuti
+        $SPIBelumDitindaklanjuti = DB::select($query, ['Satuan Pengawasan Internal', 'Belum Ditindaklanjuti']);
+        $SPIBelumDitindaklanjuti = $SPIBelumDitindaklanjuti[0]->total;
+        //Tidak Dapat Ditindaklanjuti
+        $SPITidakDapatDitindaklanjuti = DB::select($query, ['Satuan Pengawasan Internal', 'Tidak Dapat Ditindaklanjuti']);
+        $SPITidakDapatDitindaklanjuti = $SPITidakDapatDitindaklanjuti[0]->total;
+
+        //Keuangan
+        //Belum Selesai
+        $KBelumSelesai = DB::select($query, ['Keuangan', 'Belum Selesai']);
+        $KBelumSelesai = $KBelumSelesai[0]->total;
+        //Selesai
+        $KSelesai = DB::select($query, ['Keuangan', 'Selesai']);
+        $KSelesai = $KSelesai[0]->total;
+        //Belum Ditindaklanjuti
+        $KBelumDitindaklanjuti = DB::select($query, ['Keuangan', 'Belum Ditindaklanjuti']);
+        $KBelumDitindaklanjuti = $KBelumDitindaklanjuti[0]->total;
+        //Tidak Dapat Ditindaklanjuti
+        $KTidakDapatDitindaklanjuti = DB::select($query, ['Keuangan', 'Tidak Dapat Ditindaklanjuti']);
+        $KTidakDapatDitindaklanjuti = $KTidakDapatDitindaklanjuti[0]->total;
+
+        //Komersil
+        //Belum Selesai
+        $KmBelumSelesai = DB::select($query, ['Komersil', 'Belum Selesai']);
+        $KmBelumSelesai = $KmBelumSelesai[0]->total;
+        //Selesai
+        $KmSelesai = DB::select($query, ['Komersil', 'Selesai']);
+        $KmSelesai = $KmSelesai[0]->total;
+        //Belum Ditindaklanjuti
+        $KmBelumDitindaklanjuti = DB::select($query, ['Komersil', 'Belum Ditindaklanjuti']);
+        $KmBelumDitindaklanjuti = $KmBelumDitindaklanjuti[0]->total;
+        //Tidak Dapat Ditindaklanjuti
+        $KmTidakDapatDitindaklanjuti = DB::select($query, ['Komersil', 'Tidak Dapat Ditindaklanjuti']);
+        $KmTidakDapatDitindaklanjuti = $KmTidakDapatDitindaklanjuti[0]->total;
+
+        //SDM dan Umum
+        //Belum Selesai
+        $SDMBelumSelesai = DB::select($query, ['SDM dan Umum', 'Belum Selesai']);
+        $SDMBelumSelesai = $SDMBelumSelesai[0]->total;
+        //Selesai
+        $SDMSelesai = DB::select($query, ['SDM dan Umum', 'Selesai']);
+        $SDMSelesai = $SDMSelesai[0]->total;
+        //Belum Ditindaklanjuti
+        $SDMBelumDitindaklanjuti = DB::select($query, ['SDM dan Umum', 'Belum Ditindaklanjuti']);
+        $SDMBelumDitindaklanjuti = $SDMBelumDitindaklanjuti[0]->total;
+        //Tidak Dapat Ditindaklanjuti
+        $SDMTidakDapatDitindaklanjuti = DB::select($query, ['SDM dan Umum', 'Tidak Dapat Ditindaklanjuti']);
+        $SDMTidakDapatDitindaklanjuti = $SDMTidakDapatDitindaklanjuti[0]->total;
+
+        //Perencanaan dan Pengembangan
+        //Belum Selesai
+        $PPBelumSelesai = DB::select($query, ['Perencanaan dan Pengembangan', 'Belum Selesai']);
+        $PPBelumSelesai = $PPBelumSelesai[0]->total;
+        //Selesai
+        $PPSelesai = DB::select($query, ['Perencanaan dan Pengembangan', 'Selesai']);
+        $PPSelesai = $PPSelesai[0]->total;
+        //Belum Ditindaklanjuti
+        $PPBelumDitindaklanjuti = DB::select($query, ['Perencanaan dan Pengembangan', 'Belum Ditindaklanjuti']);
+        $PPBelumDitindaklanjuti = $PPBelumDitindaklanjuti[0]->total;
+        //Tidak Dapat Ditindaklanjuti
+        $PPTidakDapatDitindaklanjuti = DB::select($query, ['Perencanaan dan Pengembangan', 'Tidak Dapat Ditindaklanjuti']);
+        $PPTidakDapatDitindaklanjuti = $PPTidakDapatDitindaklanjuti[0]->total;
+
+        //Teknologi Informasi dan Sistem Manajemen
+        //Belum Selesai
+        $TIBelumSelesai = DB::select($query, ['Teknologi Informasi dan Sistem Manajemen', 'Belum Selesai']);
+        $TIBelumSelesai = $TIBelumSelesai[0]->total;
+        //Selesai
+        $TISelesai = DB::select($query, ['Teknologi Informasi dan Sistem Manajemen', 'Selesai']);
+        $TISelesai = $TISelesai[0]->total;
+        //Belum Ditindaklanjuti
+        $TIBelumDitindaklanjuti = DB::select($query, ['Teknologi Informasi dan Sistem Manajemen', 'Belum Ditindaklanjuti']);
+        $TIBelumDitindaklanjuti = $TIBelumDitindaklanjuti[0]->total;
+        //Tidak Dapat Ditindaklanjuti
+        $TITidakDapatDitindaklanjuti = DB::select($query, ['Teknologi Informasi dan Sistem Manajemen', 'Tidak Dapat Ditindaklanjuti']);
+        $TITidakDapatDitindaklanjuti = $TITidakDapatDitindaklanjuti[0]->total;
+
+
+
     	$lhpsemua = Lhp::count();
     	$bagian = Bagian::count();
-    	$temuan = Temuan::count();
+    	// $temuan = Temuan::all();
+     //    $data_temuan = Temuan::all();
+
     	return view('dashboard.index')
     	->with([
-    		'lhpsemua'	=> $lhpsemua,
-    		'bagian'	=> $bagian,
-    		'temuan'	=> $temuan,
-    		'lhprutin'	=> $lhprutin,
-    		'lhpkhusus' => $lhpkhusus,
-    		'lhpspi'	=> $lhpspi,
-    		'lhpbpkp'	=> $lhpbpkp,
-    		'lhpbpk'	=> $lhpbpk,
-    		'lhpkap'	=> $lhpkap
+    		// 'lhpsemua'	=> $lhpsemua,
+    		// 'bagian'	=> $bagian,
+    		// 'temuan'	=> $temuan,
+      //       'rekomendasiBelumSelesai' => $rekomendasiBelumSelesai,
+      //       'rekomendasiSelesai' => $rekomendasiSelesai,
+      //       'rekomendasiBelumDitindaklanjuti' => $rekomendasiBelumDitindaklanjuti,
+      //       'rekomendasiTidakDapatDitindaklanjuti' => $rekomendasiTidakDapatDitindaklanjuti,
+      //       'data_temuan' => $data_temuan,
+            //SP
+            'SPBelumSelesai'=>$SPBelumSelesai,
+            'SPSelesai' => $SPSelesai,
+            'SPBelumDitindaklanjuti' => $SPBelumDitindaklanjuti,
+            'SPTidakDapatDitindaklanjuti' => $SPTidakDapatDitindaklanjuti,
+            //SPI
+            'SPIBelumSelesai' => $SPIBelumSelesai,
+            'SPISelesai' => $SPISelesai,
+            'SPIBelumDitindaklanjuti' => $SPIBelumDitindaklanjuti,
+            'SPITidakDapatDitindaklanjuti' => $SPITidakDapatDitindaklanjuti,
+            //Keungan
+            'KBelumSelesai' => $KBelumSelesai,
+            'KSelesai' => $KSelesai,
+            'KBelumDitindaklanjuti' => $KBelumDitindaklanjuti,
+            'KTidakDapatDitindaklanjuti' => $KTidakDapatDitindaklanjuti,
+            //Komersil
+            'KmBelumSelesai' => $KmBelumSelesai,
+            'KmSelesai' => $KmSelesai,
+            'KmBelumDitindaklanjuti' => $KmBelumDitindaklanjuti,
+            'KmTidakDapatDitindaklanjuti' => $KmTidakDapatDitindaklanjuti,
+            //SDM dan umum
+            'SDMBelumSelesai' => $SDMBelumSelesai,
+            'SDMSelesai'      => $SDMSelesai,
+            'SDMBelumDitindaklanjuti' => $SDMBelumDitindaklanjuti,
+            'SDMTidakDapatDitindaklanjuti' => $SDMTidakDapatDitindaklanjuti,
+            //Perencanaan dan Pengembangan
+            'PPBelumSelesai' => $PPBelumSelesai,
+            'PPSelesai' => $PPSelesai,
+            'PPBelumDitindaklanjuti' => $PPBelumDitindaklanjuti,
+            'PPTidakDapatDitindaklanjuti' => $PPTidakDapatDitindaklanjuti,
+            //Teknologi Informasi dan Sistem Manajemen
+            'TIBelumSelesai' => $TIBelumSelesai,
+            'TISelesai' => $TISelesai,
+            'TIBelumDitindaklanjuti' => $TIBelumDitindaklanjuti,
+            'TITidakDapatDitindaklanjuti' => $TITidakDapatDitindaklanjuti,
 
     	]);
+
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 @section('welcome')
 <h4 class="mb-3 mb-md-0">
-    Keterangan LHA
+  Dashboard
 </h4>
 @stop
 @section('content2')
@@ -9,112 +9,155 @@
 <div class="row">
   <div class="col-12 col-xl-12 stretch-card">
     <div class="row flex-grow">
-      <div class="col-md-3 grid-margin stretch-card">
-        <div class="card">
+
+      <div class="col-4 grid-margin stretch-card">
+        <div class="card text-white bg-dark">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">LHA Rutin</h6>
-              
+              <div class="card-header col-12" style="text-align: center;">Sekretaris Perusahaan</div>
             </div>
+
+            <br>
             <div class="row">
-              <div class="col-6 col-md-12 col-xl-5">
-                <h3 class="mb-2">{{$lhprutin}}</h3>
+              <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">Belum Selesai <button type="button" class="btn btn-warning">{{$SPBelumSelesai}}</button> </div>
+                <div class="alert alert-success" role="alert">Selesai <button type="button" class="btn btn-success">{{$SPSelesai}}</button></div>
+                <div class="alert alert-secondary" role="alert">Belum Ditindaklanjuti <button type="button" class="btn btn-secondary">{{$SPBelumDitindaklanjuti}}</button></div>
+                <div class="alert alert-danger" role="alert">Tidak Dapat Ditindaklanjuti <button type="button" class="btn btn-danger">{{$SPTidakDapatDitindaklanjuti}}</button></div>
+                {{-- <h6>TD: {{$SPTidakDapatDitindaklanjuti}}</h6> --}}
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-3 grid-margin stretch-card">
-        <div class="card">
+
+      <div class="col-4 grid-margin stretch-card">
+        <div class="card text-white bg-dark">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">LHA Khusus</h6>
-              
+              <div class="card-header col-12" style="text-align: center;">Satuan Pengawasan Internal</div>
+
             </div>
+            <br>
             <div class="row">
-              <div class="col-6 col-md-12 col-xl-5">
-                <h3 class="mb-2">{{$lhpkhusus}}</h3>
+              <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">Belum Selesai <button type="button" class="btn btn-warning">{{$SPIBelumSelesai}}</button></div>
+                <div class="alert alert-success" role="alert">Selesai <button type="button" class="btn btn-success">{{$SPISelesai}}</button></div>
+                <div class="alert alert-secondary" role="alert">Belum Ditindaklanjuti: <button type="button" class="btn btn-secondary">{{$SPIBelumDitindaklanjuti}}</button></div>
+                <div class="alert alert-danger" role="alert">Tidak Dapat Ditindaklanjuti <button type="button" class="btn btn-danger">{{$SPITidakDapatDitindaklanjuti}}</button></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-3 grid-margin stretch-card">
-        <div class="card">
+
+      <div class="col-4 grid-margin stretch-card">
+        <div class="card text-white bg-dark">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">LHA SPI</h6>
-              
+              <div class="card-header col-12" style="text-align: center;">Keuangan</div>
+
             </div>
+            <br>
             <div class="row">
-              <div class="col-6 col-md-12 col-xl-5">
-                <h3 class="mb-2">{{$lhpspi}}</h3>
+              <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">Belum Selesai <button type="button" class="btn btn-warning">{{$KBelumSelesai}}</button></div>
+                <div class="alert alert-success" role="alert">Selesai <button type="button" class="btn btn-success">{{$KSelesai}}</button></div>
+                <div class="alert alert-secondary" role="alert">Belum Ditindaklanjuti <button type="button" class="btn btn-secondary">{{$KBelumDitindaklanjuti}}</button></div>
+                <div class="alert alert-danger" role="alert">Tidak Dapat Ditindaklanjuti <button type="button" class="btn btn-danger">{{$KTidakDapatDitindaklanjuti}}</button></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-3 grid-margin stretch-card">
-        <div class="card">
+
+      <div class="col-4 grid-margin stretch-card">
+        <div class="card text-white bg-dark">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">LHA BPKP</h6>
-              
+              <div class="card-header col-12" style="text-align: center;">Komersil</div>
+
             </div>
+            <br>
             <div class="row">
-              <div class="col-6 col-md-12 col-xl-5">
-                <h3 class="mb-2">{{$lhpbpkp}}</h3>
+              <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">Belum Selesai <button type="button" class="btn btn-warning">{{$KmBelumSelesai}}</button></div>
+                <div class="alert alert-success" role="alert">Selesai <button type="button" class="btn btn-success">{{$KmSelesai}}</button></div>
+                <div class="alert alert-secondary" role="alert">Belum Ditindaklanjuti <button type="button" class="btn btn-secondary">{{$KmBelumDitindaklanjuti}}</button></div>
+                <div class="alert alert-danger" role="alert">Tidak Dapat Ditindaklanjuti <button type="button" class="btn btn-danger">{{$KmTidakDapatDitindaklanjuti}}</button></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-3 grid-margin stretch-card">
-        <div class="card">
+
+      <div class="col-4 grid-margin stretch-card">
+        <div class="card text-white bg-dark">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">LHA BPK</h6>
-              
+              <div class="card-header col-12" style="text-align: center;">SDM & Umum</div>
+
             </div>
+            <br>
             <div class="row">
-              <div class="col-6 col-md-12 col-xl-5">
-                <h3 class="mb-2">{{$lhpbpk}}</h3>
+              <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">Belum Selesai <button type="button" class="btn btn-warning">{{$SDMBelumSelesai}}</button></div>
+                <div class="alert alert-success" role="alert">Selesai <button type="button" class="btn btn-success">{{$SDMSelesai}}</button></div>
+                <div class="alert alert-secondary" role="alert">Belum Ditindaklanjuti <button type="button" class="btn btn-secondary">{{$SDMBelumDitindaklanjuti}}</button></div>
+                <div class="alert alert-danger" role="alert">Tidak Dapat Ditindaklanjuti <button type="button" class="btn btn-danger">{{$SDMTidakDapatDitindaklanjuti}}</button></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-3 grid-margin stretch-card">
-        <div class="card">
+
+      <div class="col-4 grid-margin stretch-card">
+        <div class="card text-white bg-dark">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">LHA KAP</h6>
-              
+              <div class="card-header col-12" style="text-align: center;">Renbang</div>
+
             </div>
+            <br>
             <div class="row">
-              <div class="col-6 col-md-12 col-xl-5">
-                <h3 class="mb-2">{{$lhpkap}}</h3>
+              <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">Belum Selesai <button type="button" class="btn btn-warning">{{$PPBelumSelesai}}</button></div>
+                <div class="alert alert-success" role="alert">Selesai <button type="button" class="btn btn-success">{{$PPSelesai}}</button></div>
+                <div class="alert alert-secondary" role="alert">Belum Ditindaklanjuti <button type="button" class="btn btn-secondary">{{$PPBelumDitindaklanjuti}}</button></div>
+                <div class="alert alert-danger" role="alert">Tidak Dapat Ditindaklanjuti <button type="button" class="btn btn-danger">{{$PPTidakDapatDitindaklanjuti}}</button></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <div class="col-md-3 grid-margin stretch-card">
-        <div class="card">
+
+      <div class="col-4 grid-margin stretch-card">
+        <div class="card text-white bg-dark">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-baseline">
-              <h6 class="card-title mb-0">LHA Keseluruhan</h6>
-              
+              <div class="card-header col-12" style="text-align: center;">Teknologi Informasi</div>
+
             </div>
+            <br>
             <div class="row">
-              <div class="col-6 col-md-12 col-xl-5">
-                <h3 class="mb-2">{{$lhpsemua}}</h3>
+              <div class="col-md-12">
+                <div class="alert alert-warning" role="alert">Belum Selesai <button type="button" class="btn btn-warning">{{$TIBelumSelesai}}</button></div>
+                <div class="alert alert-success" role="alert">Selesai <button type="button" class="btn btn-success">{{$TISelesai}}</button></div>
+                <div class="alert alert-secondary" role="alert">Belum Ditindaklanjuti <button type="button" class="btn btn-secondary">{{$TIBelumDitindaklanjuti}}</button></div>
+                <div class="alert alert-danger" role="alert">Tidak Dapat Ditindaklanjuti <button type="button" class="btn btn-danger">{{$TITidakDapatDitindaklanjuti}}</button></div>
               </div>
             </div>
           </div>
         </div>
       </div>
+
     </div>
   </div>
-</div> <!-- row -->
+</div>
+
+<hr class="col-xl-12">
+
+<div id="chart"></div>
+<script src="/js/highcharts.js"></script>
+
 @stop
